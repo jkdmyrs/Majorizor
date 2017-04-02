aa19386145e808c89855371c54cfd47462d5a7d8
Jackson DeMeyers <jackson.demeyers@gmail.com>
2017-04-02 19:43:58 -0400

    Time class

---
+++ Majorizor/Resources/Time.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Majorizor.Resources
+{
+    public class Time
+    {
+        public int Hours;
+        public int Minutes;
+
+        public Time(int h, int m)
+        {
+            if (h > 23 || m > 59)
+            {
+                throw new ArgumentException("Invalid time specified");
+            }
+            Hours = h;
+            Minutes = m;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(
+                "{0:00}:{1:00}:",
+                this.Hours, this.Minutes);
+        }
+    }
+}
