3fe5675fe176e1dafcecc220a5e061dd4241a907
Joseph Mortefolio <Joseph Mortefolio>
2017-04-02 20:11:15 -0400

    Course class, masterscheduleparser class

---
+++ Majorizor/Resources/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Majorizor.Resources
+{
+    public class Course
+    {
+        public int id;
+        public string subject;
+        public int catalog;
+        public int section;
+        public string name;
+        public Time startTime;
+        public Time endTime;
+        public string days;
+
+    }
+}
---
+++ Majorizor/Resources/MasterScheduleparser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Majorizor.Resources
+{
+    public class MasterScheduleparser
+    {
+        public List<Course> EE;
+        public List<Course> CE;
+        public List<Course> SE;
+        public List<Course> CS;
+        public List<Course> MA;
+        public void parse(string filename)
+        {
+            TextFieldParser parser = new TextFieldParser(filename);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            while(!parser.EndOfData)
+            {
+
+            }
+
+        }
+
+
+
+    }
+}
