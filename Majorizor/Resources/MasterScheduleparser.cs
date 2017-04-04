f91afa9616272e857388176672b9b64c638e4c2b
Joseph Mortefolio <Joseph Mortefolio>
2017-04-04 12:33:18 -0400

    Course class, MasterScheduleparser class

--- Majorizor/Resources/MasterScheduleparser.cs
+++ Majorizor/Resources/MasterScheduleparser.cs
@@ -3,27 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
-//using Microsoft.VisualBasic.FileIO;
+
 
 namespace Majorizor.Resources
 {
     public class MasterScheduleparser
     {
-        //public List<Course> EE;
-        //public List<Course> CE;
-        //public List<Course> SE;
-        //public List<Course> CS;
-        //public List<Course> MA;
-        //public void parse(string filename)
-        //{
-        //    TextFieldParser parser = new TextFieldParser(filename);
-        //    parser.TextFieldType = FieldType.Delimited;
-        //    parser.SetDelimiters(",");
-        //    while(!parser.EndOfData)
-        //    {
+        public List<Course> EE;
+        public List<Course> CE;
+        public List<Course> SE;
+        public List<Course> CS;
+        public List<Course> MA;
+        public MasterScheduleparser(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+            string line;
+            while(!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                string[] lineElements = line.Split(',');
+                if (lineElements[1] == "EE")
+                {
+                    int id = Convert.ToInt16(lineElements[0]);
+                    int catalog = Convert.ToInt16(lineElements[2]);
+                    int section = Convert.ToInt16(lineElements[3]);
+                    DateTime start = Convert.ToDateTime(lineElements[8]);
+                    DateTime end = Convert.ToDateTime(lineElements[9]);
+                    Course currentCourse = new Course(id, lineElements[1], catalog, section, lineElements[4], start, end, lineElements[10]);
+                    EE.Add(currentCourse);
+                }
+                if (lineElements[1] == "CS")
+                {
+                    int id = Convert.ToInt16(lineElements[0]);
+                    int catalog = Convert.ToInt16(lineElements[2]);
+                    int section = Convert.ToInt16(lineElements[3]);
+                    DateTime start = Convert.ToDateTime(lineElements[8]);
+                    DateTime end = Convert.ToDateTime(lineElements[9]);
+                    Course currentCourse = new Course(id, lineElements[1], catalog, section, lineElements[4], start, end, lineElements[10]);
+                    CS.Add(currentCourse);
+                }
+                if (lineElements[1] == "MA")
+                {
+                    int id = Convert.ToInt16(lineElements[0]);
+                    int catalog = Convert.ToInt16(lineElements[2]);
+                    int section = Convert.ToInt16(lineElements[3]);
+                    DateTime start = Convert.ToDateTime(lineElements[8]);
+                    DateTime end = Convert.ToDateTime(lineElements[9]);
+                    Course currentCourse = new Course(id, lineElements[1], catalog, section, lineElements[4], start, end, lineElements[10]);
+                    MA.Add(currentCourse);
+                }
 
-        //    }
-        //}
+
+            }
+
+        }
+
 
 
 
