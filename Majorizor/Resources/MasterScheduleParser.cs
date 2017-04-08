2e1a1d4e510a48ff5d7cdc1a98f9847954d1ebd0
Joseph Mortefolio <Joseph Mortefolio>
2017-04-08 15:40:48 -0400

    Course class, MasterScheduleparser class

--- Majorizor/Resources/MasterScheduleParser.cs
+++ Majorizor/Resources/MasterScheduleParser.cs
@@ -19,12 +19,14 @@ namespace Majorizor.Resources
         {
             StreamReader reader = new StreamReader(scheduleStream);
             string line;
+            string NewMasterSchedule = " ";
             while(!reader.EndOfStream)
             {
                 line = reader.ReadLine();
                 string[] lineElements = line.Split('|');
                 if (lineElements[1] == "CS")
                 {
+                   NewMasterSchedule = NewMasterSchedule.Insert(0, line + Environment.NewLine);
                     int id = Convert.ToInt16(lineElements[0]);
                     int catalog = Convert.ToInt16(lineElements[2]);
                     if (lineElements[8] != "")
@@ -49,6 +51,7 @@ namespace Majorizor.Resources
                 }
                 if (lineElements[1] == "EE")
                 {
+                   NewMasterSchedule = NewMasterSchedule.Insert(0, line + Environment.NewLine);
                     int id = Convert.ToInt16(lineElements[0]);
                     int catalog = Convert.ToInt16(lineElements[2]);
                     if (lineElements[8] != "")
@@ -73,6 +76,7 @@ namespace Majorizor.Resources
                 }
                 if (lineElements[1] == "MA")
                 {
+                   NewMasterSchedule =  NewMasterSchedule.Insert(0, line + Environment.NewLine);
                     int id = Convert.ToInt16(lineElements[0]);
                     int catalog = Convert.ToInt16(lineElements[2]);
                     if (lineElements[8] != "")
@@ -93,12 +97,42 @@ namespace Majorizor.Resources
                         Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, "NA");
                         MA.Add(currentCourse);
                     }
+                   
+
+                    }
+                if ((lineElements[1] == "CM"  && ((lineElements[2] == "131") || (lineElements[2] == "132"))) || (lineElements[1]=="PH" &&  ((lineElements[2] == "131") || (lineElements[2] == "132"))) )
+                {
+                    NewMasterSchedule =  NewMasterSchedule.Insert(0, line + Environment.NewLine);
+                    int id = Convert.ToInt16(lineElements[0]);
+                    int catalog = Convert.ToInt16(lineElements[2]);
+                    if (lineElements[8] != "")
+                    {
+                        DateTime start = DateTime.Parse(lineElements[8]);
+                        start.ToShortTimeString();
+                        DateTime end = DateTime.Parse(lineElements[9]);
+                        end.ToShortTimeString();
+                        Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, lineElements[10]);
+                        EE.Add(currentCourse);
+                        CE.Add(currentCourse);
+                        SE.Add(currentCourse);
+                    }
+                    else
+                    {
+                        DateTime start = Convert.ToDateTime("23:58:59");
+                        start.ToShortTimeString();
+                        DateTime end = Convert.ToDateTime("23:59:59");
+                        end.ToShortTimeString();
+                        Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, "NA");
+                        EE.Add(currentCourse);
+                        CE.Add(currentCourse);
+                        SE.Add(currentCourse);
+                    }
+
+
 
                 }
+                }
 
-
-
-            }
 
         }
 
