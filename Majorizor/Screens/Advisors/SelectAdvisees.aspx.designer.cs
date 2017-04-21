37f9fce6d9cde286b6d818a19edcfacfff8b1baa
Jack DeMeyers <jackson.demeyers@gmail.com>
2017-04-21 16:15:08 -0400

    Removing ProgressTracker class temporarily

--- Majorizor/Screens/Advisors/SelectAdvisees.aspx.designer.cs
+++ Majorizor/Screens/Advisors/SelectAdvisees.aspx.designer.cs
@@ -11,32 +11,5 @@ namespace Majorizor.Screens.Advisors {
     
     
     public partial class SelectAdvisees {
-        
-        /// <summary>
-        /// checkBoxList_advisees control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBoxList checkBoxList_advisees;
-        
-        /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
-        /// button_save control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button button_save;
     }
 }
