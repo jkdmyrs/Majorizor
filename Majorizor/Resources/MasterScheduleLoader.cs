eda86641f76929347ed786ea8ce11013f2d722c2
Jackson DeMeyers <jackson.demeyers@gmail.com>
2017-04-24 20:53:45 -0400

    Exception handling fix

--- Majorizor/Resources/MasterScheduleLoader.cs
+++ Majorizor/Resources/MasterScheduleLoader.cs
@@ -32,62 +32,70 @@ namespace Majorizor.Resources
         /// </summary>
         private void ProcessScheduleStream()
         {
-            string line;
-            while (!reader.EndOfStream)
+            try
             {
-                line = reader.ReadLine();
-                string[] lineElements = line.Split('|');
-                if (MajorList.Contains(lineElements[1]))
+                string line;
+                while (!reader.EndOfStream)
                 {
-                    int id = Convert.ToInt16(lineElements[0]);
-                    string catalog = lineElements[2];
-                    if (lineElements[8] != "")
+                    line = reader.ReadLine();
+                    string[] lineElements = line.Split('|');
+                    if (MajorList.Contains(lineElements[1]))
                     {
-                        DateTime start = DateTime.Parse(lineElements[8]);
-                        start.ToShortTimeString();
-                        DateTime end = DateTime.Parse(lineElements[9]);
-                        end.ToShortTimeString();
-                        Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, lineElements[10]);
-                        ParsedMasterSchedule.Add(currentCourse);
+                        int id = Convert.ToInt16(lineElements[0]);
+                        string catalog = lineElements[2];
+                        if (lineElements[8] != "")
+                        {
+                            DateTime start = DateTime.Parse(lineElements[8]);
+                            start.ToShortTimeString();
+                            DateTime end = DateTime.Parse(lineElements[9]);
+                            end.ToShortTimeString();
+                            Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, lineElements[10]);
+                            ParsedMasterSchedule.Add(currentCourse);
+                        }
+                        else
+                        {
+                            DateTime start = Convert.ToDateTime("23:58:59");
+                            start.ToShortTimeString();
+                            DateTime end = Convert.ToDateTime("23:59:59");
+                            end.ToShortTimeString();
+                            Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, "NA");
+                            ParsedMasterSchedule.Add(currentCourse);
+                        }
                     }
-                    else
-                    {
-                        DateTime start = Convert.ToDateTime("23:58:59");
-                        start.ToShortTimeString();
-                        DateTime end = Convert.ToDateTime("23:59:59");
-                        end.ToShortTimeString();
-                        Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, "NA");
-                        ParsedMasterSchedule.Add(currentCourse);
-                    }
-                }
 
-                if ((lineElements[1] == "CM" && ((lineElements[2] == "131") || (lineElements[2] == "132"))) ||
-                        (lineElements[1] == "PH" && ((lineElements[2] == "131") || (lineElements[2] == "132"))) ||
-                        (lineElements[1] == "ES" && (ES_ElectiveList.Contains(lineElements[2]))) ||
-                        (lineElements[1] == "UNIV" && (lineElements[2] == "190")) ||
-                        (lineElements[1] == "STAT" && (lineElements[2] == "383")))
-                {
-                    int id = Convert.ToInt16(lineElements[0]);
-                    string catalog = lineElements[2];
-                    if (lineElements[8] != "")
+                    if ((lineElements[1] == "CM" && ((lineElements[2] == "131") || (lineElements[2] == "132"))) ||
+                            (lineElements[1] == "PH" && ((lineElements[2] == "131") || (lineElements[2] == "132"))) ||
+                            (lineElements[1] == "ES" && (ES_ElectiveList.Contains(lineElements[2]))) ||
+                            (lineElements[1] == "UNIV" && (lineElements[2] == "190")) ||
+                            (lineElements[1] == "STAT" && (lineElements[2] == "383")))
                     {
-                        DateTime start = DateTime.Parse(lineElements[8]);
-                        start.ToShortTimeString();
-                        DateTime end = DateTime.Parse(lineElements[9]);
-                        end.ToShortTimeString();
-                        Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, lineElements[10]);
-                        ParsedMasterSchedule.Add(currentCourse);
-                    }
-                    else
-                    {
-                        DateTime start = Convert.ToDateTime("23:58:59");
-                        start.ToShortTimeString();
-                        DateTime end = Convert.ToDateTime("23:59:59");
-                        end.ToShortTimeString();
-                        Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, "NA");
-                        ParsedMasterSchedule.Add(currentCourse);
+                        int id = Convert.ToInt16(lineElements[0]);
+                        string catalog = lineElements[2];
+                        if (lineElements[8] != "")
+                        {
+                            DateTime start = DateTime.Parse(lineElements[8]);
+                            start.ToShortTimeString();
+                            DateTime end = DateTime.Parse(lineElements[9]);
+                            end.ToShortTimeString();
+                            Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, lineElements[10]);
+                            ParsedMasterSchedule.Add(currentCourse);
+                        }
+                        else
+                        {
+                            DateTime start = Convert.ToDateTime("23:58:59");
+                            start.ToShortTimeString();
+                            DateTime end = Convert.ToDateTime("23:59:59");
+                            end.ToShortTimeString();
+                            Course currentCourse = new Course(id, lineElements[1], catalog, lineElements[3], lineElements[4], start, end, "NA");
+                            ParsedMasterSchedule.Add(currentCourse);
+                        }
                     }
                 }
+            }
+            catch (Exception e)
+            {
+                string error = "MasterSheduleLoader.ProcessScheduleStream failed with error: " + e.Message;
+                throw new Exception(error);
             }
         }
 
